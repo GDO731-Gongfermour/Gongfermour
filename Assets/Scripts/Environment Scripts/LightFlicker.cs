99d2c2dd7d8c691a6cfd1f28c9be769d1e18c3d2
iArkas <matthewsowden@outlook.com>
2026-07-13 15:50:20 +0100

    added light flicker script + tidied up project files

---
+++ Assets/Scripts/Environment Scripts/LightFlicker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light light;
+    private bool inf = true;
+
+    void Start()
+    {            
+        StartCoroutine(ChangeIntensity());
+    }
+
+    private IEnumerator ChangeIntensity()
+    {
+        while (inf)
+        {
+            float startIntensity = light.intensity;
+            float targetIntensity = Random.Range(0.5f, 2f);
+
+            float startRange = light.range;
+            float targetRange = Random.Range(2, 7);
+
+            float duration = 0.5f;
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+                float t = Mathf.SmoothStep(0f, 1f, elapsed / duration);
+
+                light.intensity = Mathf.Lerp(startIntensity, targetIntensity, t);
+                light.range = Mathf.Lerp(startRange, targetRange, t);
+
+                yield return null;
+            }
+        }
+    }
+}
